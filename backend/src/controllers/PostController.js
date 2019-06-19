1cc439ca4af0cd72fb0883b493029cedd71d97f7
fernando.js8 <fernando.js8@outlook.com>
2019-06-19 09:39:33 -0300

    start

---
+++ backend/src/controllers/PostController.js
@@ -0,0 +1,36 @@
+const Post  = require('../models/Post');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
+
+module.exports = {
+    async index(req, res) {
+        const posts = await Post.find().sort('-createdAt');
+
+        return res.json(posts);
+    },
+
+    async store(req, res) {
+        const { author, place, description, hastags } = req.body;
+        const { filename: image } = req.file;
+
+        await sharp(req.file.path)
+            .resize(500)
+            .jpeg( { quality: 70 })
+            .toFile(
+                path.resolve(req.file.destination, 'resized', image)
+            )
+
+        const post = await Post.create({
+            author,
+            place,
+            description,
+            hastags,
+            image,
+        })
+
+        fs.unlinkSync(req.file.path);
+
+        return res.json(post);
+    }
+};
---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://ferjs:ferjsaula@cluster0-inuh7.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+});
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
+app.use(require('./routes'));
+
+app.listen(3333);
